f96a99a31d8f024ab4146c511648ea0623db773b
Chaz Henricks <chazhenricks@gmail.com>
2017-07-11 19:58:03 -0500

    added comments

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace family_dictionary
     {
         static void Main(string[] args)
         {
+            //creates a new dictionary that has a string as a key and another dictionary as a value
             Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
 
             myFamily.Add("mother", new Dictionary<string, string>(){ 
@@ -29,6 +30,10 @@ namespace family_dictionary
                 {"age", "25"}
             });
             
+            //iterates over the dictionary. 
+            //member.Key references the relationship("brother", "mother"...)
+            //member.Value["name"] refers to their name
+            //member.Value["age"] refers to their age 
             foreach(KeyValuePair<string, Dictionary<string, string>> member in myFamily){
                 Console.WriteLine($"{member.Value["name"]} is my {member.Key} and is {member.Value["age"]} years old");
 
